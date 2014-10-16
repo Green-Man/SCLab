4d0d6a55c2e57ab6db23b8fe6e89eaa1af181952
Green-Man <greenman.mail@gmail.com>
2014-10-16 22:09:56 +0200

    Heun method works

--- w01_euler.m
+++ w01_euler.m
@@ -1,11 +1,11 @@
-function [ y_euler ] = w01_euler( fy, y0, t0, dt, t_end )
+function [ y ] = w01_euler( fy, y0, t0, dt, t_end )
 %W01_EULER explicit Euler method
-    y_euler = zeros(1,size(0:dt:t_end,2));
-    y_euler(1) = y0;
+    y = zeros(1,size(0:dt:t_end,2));
+    y(1) = y0;
     
-    for n = 2:size(y_euler,2)
-        dpdt = fy(y_euler(n-1));
-        y_euler(n) = y_euler(n-1) + dt*dpdt;
+    for n = 2:size(y,2)
+        dydt = fy(y(n-1));
+        y(n) = y(n-1) + dt*dydt;
     end
 
 end
---
+++ w01_heun.m
@@ -0,0 +1,14 @@
+function [ y ] = w01_huen( fy, y0, t0, dt, t_end )
+%W01_HUEN method Heun
+    y = zeros(1,size(0:dt:t_end,2));
+    y(1) = y0;
+    
+    for n = 2:size(y,2)
+        dydt1 = fy(y(n-1));
+        dydt2 = fy(y(n));
+        y(n) = y(n-1) + dt*(dydt1+dydt2)/2;
+    end
+
+
+end
+
