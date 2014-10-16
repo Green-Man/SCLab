082fb6c420989a8f2cd2bc4662374ccdc8483798
Green-Man <greenman.mail@gmail.com>
2014-10-16 21:41:46 +0200

    Euler works

--- w01_euler.m
+++ w01_euler.m
@@ -1,11 +1,11 @@
-function [ y_euler ] = w01_euler( fy, y0, dt, t_end )
+function [ y_euler ] = w01_euler( fy, y0, t0, dt, t_end )
 %W01_EULER explicit Euler method
-    y_euler = zeros(1,t_end/dt+1);
+    y_euler = zeros(1,size(0:dt:t_end,2));
     y_euler(1) = y0;
-    y_euler=y_euler.^2;
     
     for n = 2:size(y_euler,2)
-        y_euler(n) = n.^0.5;
+        dpdt = fy(y_euler(n-1));
+        y_euler(n) = y_euler(n-1) + dt*dpdt;
     end
 
 end
