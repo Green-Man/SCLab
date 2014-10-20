61e62492935e1c29f61330744a729dd0c4c146f1
Green-Man <greenman.mail@gmail.com>
2014-10-20 23:33:48 +0200

    Reduction of the error added

--- worksheet1.m
+++ worksheet1.m
@@ -4,7 +4,7 @@ hold on;
 
 %Time boundaries
 t0 =0;
-dts = [1/1 1/2 1/4 1/8];
+dts = [1/1 1/2 1/4 1/8 1/16];
 t_end = 10.^1;
 
 
@@ -14,11 +14,13 @@ p0 = 1;
 
 methods = {'euler', 'heun', 'rk'};
 i = 1;
+ae = ones(size(methods,2), size(dts,2));
 for method = methods
     figure(1)
     hold on
     subplot(size(methods,2),1,i);
     legendStrings = {'Analytic'};
+    j=1;
     for dt = dts
 
         T = t0:dt:t_end;
@@ -36,22 +38,34 @@ for method = methods
         plot(T, ns, 'Color', gColor);
         hold on;
         
-        
-    %     s = l.TextColor;
-    %     l.TextColor = 'red';%{sprintf('dt = %f', dt),};
-    %     legend(sprintf('dt = %f', dt), 'Location','northoutside','Orientation','horizontal')
-
         %Plot absolute errors
 %         plot(T, ns-P, 'Color', [dt^0.5 0 0]);
 
         %Compute approximation error
-        ape = @(N) sqrt(sum((N-P).^2)*dt/5);
-        legendStrings = [legendStrings, {sprintf('dt=%2.3f ae=%2.6f', dt, ape(ns))}];
+        approxError = @(N) sqrt(sum((N-P).^2)*dt/5);
+        ae(i,j) = approxError(ns);
+        legendStrings = [legendStrings, {sprintf('dt=%2.3f ae=%2.6f', dt, ae(i,j))}];
+        j = j + 1;
     end
     
     legend(legendStrings)
     title(method);
     i=i+1;
 end
+
+%compute reduction error when "decreasing dt at two times"
+re = ones(size(ae,1),size(ae,1)-2);
+for method = 1:size(ae,1)
+    for dt = 2:size(dts,2)
+        re(method, dt-1) = ae(method, dt-1)/ae(method, dt);
+    end
+end
+mean(re,2)
+
+%plot reduction error
+% figure(2)
+% plot(dts(2:size(dts,2)), re')
+% axis([min(dts) max(dts) 0 17]);
+
 
 hold off;
