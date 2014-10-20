9d227f02be9fcbf3af97bedbe5959b6b1b460c8e
Green-Man <greenman.mail@gmail.com>
2014-10-20 11:04:32 +0200

    Nice graphs

--- worksheet1.m
+++ worksheet1.m
@@ -3,62 +3,55 @@ clf;
 hold on;
 
 %Time boundaries
-t0 =0;dts = [1/1 1/2 1/4 1/8];t_end = 10.^1;
-dts = [1/2];
+t0 =0;
+dts = [1/1 1/2 1/4 1/8];
+t_end = 10.^1;
+
 
 %Given population ODE and initial condition
 dpdt = @(p) (1 - p/10)*p;
 p0 = 1;
 
-for dt = dts
-    %Execution of numerical methods
-    eu = numerical( dpdt, p0, t0, dt, t_end , 'euler');
-    hu = numerical( dpdt, p0, t0, dt, t_end , 'heun');
-    rk = numerical( dpdt, p0, t0, dt, t_end , 'rk');
-    
-    T = t0:dt:t_end;
-    %Analytical solution and its plot
-    p = @(t) 10 ./ (1+9*exp(-t));
-    P = p(T);
-    
-    %Plots of numerical methods
+methods = {'euler', 'heun', 'rk'};
+i = 1;
+for method = methods
     figure(1)
-    
-    subplot(3,1,1);
-    
-%     l = legend('show');
-    plot(T, P, 'Color', [0.8 0.8 0.8]);
-    plot(T, eu, 'Color', [dt^0.5 0 0]);
+    hold on
+    subplot(size(methods,2),1,i);
+    legendStrings = {'Analytic'};
+    for dt = dts
 
+        T = t0:dt:t_end;
+        %Analytical solution and its plot
+        p = @(t) 10 ./ (1+9*exp(-t));
+        P = p(T);
+        plot(T, P, 'Color', [0.8 0.8 0.8]);
+        
+        %Execution of numerical methods
+        ns = numerical( dpdt, p0, t0, dt, t_end , method);
+        
+        %Plots of numerical methods
+        gColor = [0,0,0]; gColor(i) = dt^0.5;
+        hold on;
+        plot(T, ns, 'Color', gColor);
+        hold on;
+        
+        
+    %     s = l.TextColor;
+    %     l.TextColor = 'red';%{sprintf('dt = %f', dt),};
+    %     legend(sprintf('dt = %f', dt), 'Location','northoutside','Orientation','horizontal')
 
-    title('Euler')
-%     s = l.TextColor;
-%     l.TextColor = 'red';%{sprintf('dt = %f', dt),};
-%     legend(sprintf('dt = %f', dt), 'Location','northoutside','Orientation','horizontal')
-    
-    subplot(3,1,2);
-    h = plot(T, P, 'Color', [0.8 0.8 0.8]);
-    plot(T, hu, 'Color', [0 dt^0.5 0]);
-    title('Heun')
-%     legend(dts)
-%     
-    subplot(3,1,3);
-	plot(T, P, 'Color', [0.8 0.8 0.8]);
-	plot(T, rk, 'Color', [0 0 dt^0.5]);
-    title('Runge-Kutta')
-%     legend(dts)
+        %Plot absolute errors
+%         plot(T, ns-P, 'Color', [dt^0.5 0 0]);
 
+        %Compute approximation error
+        ape = @(N) sqrt(sum((N-P).^2)*dt/5);
+        legendStrings = [legendStrings, {sprintf('dt=%2.3f ae=%2.6f', dt, ape(ns))}];
+    end
     
-
-    %Plot absolute errors
-%     plot(T, eu-P, 'Color', [dt^0.5 0 0]);
-%     plot(T, hu-P, 'Color', [0 dt^0.5 0]);
-%     plot(T, rk-P, 'Color', [0 0 1]);
-    
-    %Compute approximation error
-    ape = @(N) sqrt(sum((N-P).^2)*dt/5);
-    ape(eu);
-    
+    legend(legendStrings)
+    title(method);
+    i=i+1;
 end
 
 hold off;
