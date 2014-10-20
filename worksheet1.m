cb5295ca4ee66865aebd08242a71ad7e5c9bd372
Green-Man <greenman.mail@gmail.com>
2014-10-20 10:24:36 +0200

    plot 3in1 WIP

--- worksheet1.m
+++ worksheet1.m
@@ -3,28 +3,62 @@ clf;
 hold on;
 
 %Time boundaries
-t0 =0;dt = 10.^-1;t_end = 10.^1;
-T = t0:dt:t_end;
-iter_to_plot = round(t_end/dt);
-
-%Analytical solution and its plot
-p = @(t) 10 ./ (1+9*exp(-t));
-P = p(T);
-% plot(T(1:iter_to_plot), P(1:iter_to_plot), 'Color', [0.8 0.8 0.8]);
+t0 =0;dts = [1/1 1/2 1/4 1/8];t_end = 10.^1;
+dts = [1/2];
 
 %Given population ODE and initial condition
 dpdt = @(p) (1 - p/10)*p;
 p0 = 1;
 
-%Execution of numerical methods
-eu = numerical( dpdt, p0, t0, dt, t_end , 'euler') - P;
-hu = numerical( dpdt, p0, t0, dt, t_end , 'heun') - P;
-rk = numerical( dpdt, p0, t0, dt, t_end , 'rk') - P;
+for dt = dts
+    %Execution of numerical methods
+    eu = numerical( dpdt, p0, t0, dt, t_end , 'euler');
+    hu = numerical( dpdt, p0, t0, dt, t_end , 'heun');
+    rk = numerical( dpdt, p0, t0, dt, t_end , 'rk');
+    
+    T = t0:dt:t_end;
+    %Analytical solution and its plot
+    p = @(t) 10 ./ (1+9*exp(-t));
+    P = p(T);
+    
+    %Plots of numerical methods
+    figure(1)
+    
+    subplot(3,1,1);
+    
+%     l = legend('show');
+    plot(T, P, 'Color', [0.8 0.8 0.8]);
+    plot(T, eu, 'Color', [dt^0.5 0 0]);
 
-%Plots of numerical methods
-plot(T(1:iter_to_plot), eu(1:iter_to_plot), 'Color', [1 0 0]);
-plot(T(1:iter_to_plot), hu(1:iter_to_plot), 'Color', [0 1 0]);
-plot(T(1:iter_to_plot), rk(1:iter_to_plot), 'Color', [0 0 1]);
 
+    title('Euler')
+%     s = l.TextColor;
+%     l.TextColor = 'red';%{sprintf('dt = %f', dt),};
+%     legend(sprintf('dt = %f', dt), 'Location','northoutside','Orientation','horizontal')
+    
+    subplot(3,1,2);
+    h = plot(T, P, 'Color', [0.8 0.8 0.8]);
+    plot(T, hu, 'Color', [0 dt^0.5 0]);
+    title('Heun')
+%     legend(dts)
+%     
+    subplot(3,1,3);
+	plot(T, P, 'Color', [0.8 0.8 0.8]);
+	plot(T, rk, 'Color', [0 0 dt^0.5]);
+    title('Runge-Kutta')
+%     legend(dts)
+
+    
+
+    %Plot absolute errors
+%     plot(T, eu-P, 'Color', [dt^0.5 0 0]);
+%     plot(T, hu-P, 'Color', [0 dt^0.5 0]);
+%     plot(T, rk-P, 'Color', [0 0 1]);
+    
+    %Compute approximation error
+    ape = @(N) sqrt(sum((N-P).^2)*dt/5);
+    ape(eu);
+    
+end
 
 hold off;
